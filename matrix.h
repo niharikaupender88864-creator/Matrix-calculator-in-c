114f26f823c4c667dcc6e9da650458cae2beee2e
niharikaupender88864-creator <niharikaupender88864@gmail.com>
2026-06-27 19:46:49 +0530

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -1,56 +1,28 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
-#define MAX 10
-
-/* Global Matrices */
-
-extern int A[MAX][MAX];
-extern int B[MAX][MAX];
-extern int Result[MAX][MAX];
-
-/* Matrix Dimensions */
-
-extern int rows1, cols1;
-extern int rows2, cols2;
-
-/* Input & Output */
-
-void inputMatrix(int matrix[MAX][MAX], int rows, int cols);
-void displayMatrix(int matrix[MAX][MAX], int rows, int cols);
-
-/* Basic Operations */
-
+/* Matrix Arithmetic Operations */
 void addition();
 void subtraction();
 void multiplication();
 
-/* Advanced Operations */
-
+/* Matrix Operations */
 void transpose();
-void traceMatrix();
-
-int determinant(int matrix[MAX][MAX], int n);
-
+void determinant();
 void inverse();
 
-/* Classification */
+/* Matrix Classification */
+void classify();
 
-void classifyMatrix();
-
-/* Export */
-
+/* Export Result */
 void exportResult();
 
+/* Input and Output Functions */
+void inputMatrix(int matrix[10][10], int rows, int cols);
+void displayMatrix(int matrix[10][10], int rows, int cols);
+
 /* Utility Functions */
-
-void clearMatrix(int matrix[MAX][MAX], int rows, int cols);
-
-void copyMatrix(int source[MAX][MAX],
-                int destination[MAX][MAX],
-                int rows,
-                int cols);
-
 void printLine();
+void printTitle(char title[]);
 
 #endif
